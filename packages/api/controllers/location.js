cc007db9aa93cf7e9c212a8b71a5184cf1b07a74
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 16:50:20 -0600

    Emit socket events on create, update and delete

--- packages/api/controllers/location.js
+++ packages/api/controllers/location.js
@@ -1,16 +1,30 @@
 const { Location } = require("../models");
+const {
+  SEQUELIZE_VALIDATION_ERROR,
+  LOCATIONS_CHANNEL_NAME,
+  CREATE,
+  UPDATE,
+  DELETE,
+} = require('../constants')
 
-const SEQUELIZE_VALIDATION_ERROR = "SequelizeValidationError";
+function emitChannelEvent(io, eventType, id) {
+  const body = {
+    eventType,
+    locationId: id,
+  }
+
+  io.of(LOCATIONS_CHANNEL_NAME).emit(JSON.stringify(body))
+}
 
 module.exports = {
   /**
-  * Get location list
-  * 
-  * @function
-  * @param req {Object} The request.
-  * @param res {Object} The response.
-  * @return {undefined}
-  */
+   * Get location list
+   *
+   * @function
+   * @param req {Object} The request.
+   * @param res {Object} The response.
+   * @return {undefined}
+   */
   async get(req, res) {
     try {
       const locations = await Location.findAll();
@@ -18,21 +32,19 @@ module.exports = {
     } catch (error) {
       console.error("Error: location:list:", error);
 
-      res
-        .status(500)
-        .send("There was an error getting the location list");
+      res.status(500).send("There was an error getting the location list");
     }
   },
- 
+
   /**
-    * Get location given an id
-    *
-    * @function
-    * @param req {Object} The request.
-    * @param res {Object} The response.
-    * @param req.params.id {String} Location id
-    * @return {undefined}
-    */
+   * Get location given an id
+   *
+   * @function
+   * @param req {Object} The request.
+   * @param res {Object} The response.
+   * @param req.params.id {String} Location id
+   * @return {undefined}
+   */
   async getById(req, res) {
     const locationId = req.params.id;
 
@@ -42,9 +54,7 @@ module.exports = {
       if (location) {
         res.json(location);
       } else {
-        res
-          .status(404)
-          .send("Location not found");
+        res.status(404).send("Location not found");
       }
     } catch (error) {
       console.error(`Error: location:get ${locationId}:`, error);
@@ -54,7 +64,7 @@ module.exports = {
         .send(`There was an error getting the location ${locationId}`);
     }
   },
- 
+
   /**
    * Create location
    *
@@ -65,26 +75,28 @@ module.exports = {
    * @return {undefined}
    */
   async create(req, res) {
+    const io = req.app.get('io');
+
     try {
       const location = await Location.create(req.body);
       res.json(location);
+
+      emitChannelEvent(io, CREATE, location.id)
     } catch (error) {
-      console.error('Error: location:create:', error);
-      
+      console.error("Error: location:create:", error);
+
       if (error.name === SEQUELIZE_VALIDATION_ERROR) {
         res
           .status(400)
-          .send(error.errors.map(({ message }) => message).join(' | '))
+          .send(error.errors.map(({ message }) => message).join(" | "));
 
-        return
+        return;
       }
 
-      res
-        .status(500)
-        .send('There was an error creation the location');
+      res.status(500).send("There was an error creation the location");
     }
   },
-  
+
   /**
    * Update location given an id and the new parameters to be updated
    *
@@ -96,6 +108,8 @@ module.exports = {
    * @return {undefined}
    */
   async update(req, res) {
+    const io = req.app.get('io');
+
     const { body } = req;
     const locationId = req.params.id;
 
@@ -103,23 +117,22 @@ module.exports = {
       const location = await Location.findByPk(req.params.id);
 
       if (!location) {
-        res
-          .status(404)
-          .send("Location not found");
-        
-        return
+        res.status(404).send("Location not found");
+
+        return;
       }
 
-      await location.update(body, { fields: Object.keys(body) })
+      await location.update(body, { fields: Object.keys(body) });
 
-      res.json(location)
+      res.json(location);
+      emitChannelEvent(io, UPDATE, location.id)
     } catch (error) {
-      if (error.name === SEQUELIZE_VALIDATION_ERROR) { 
+      if (error.name === SEQUELIZE_VALIDATION_ERROR) {
         res
           .status(400)
-          .send(error.errors.map(({ message }) => message).join(' | '))
+          .send(error.errors.map(({ message }) => message).join(" | "));
 
-        return 
+        return;
       }
 
       console.error(`Error: location:update ${locationId}:`, error);
@@ -129,34 +142,34 @@ module.exports = {
         .send(`There was an error updating the location ${locationId}`);
     }
   },
-  
+
   /**
-  * Delete location given an id
-  *
-  * @function
-  * @param req {Object} The request.
-  * @param req.params.id {Number} Location id to be deleted.
-  * @param res {Object} The response.
-  * @return {undefined}
-  */
+   * Delete location given an id
+   *
+   * @function
+   * @param req {Object} The request.
+   * @param req.params.id {Number} Location id to be deleted.
+   * @param res {Object} The response.
+   * @return {undefined}
+   */
   async delete(req, res) {
+    const io = req.app.get('io')
     const locationId = req.params.id;
 
     try {
       const location = await Location.findByPk(req.params.id);
 
       if (!location) {
-        res
-          .status(404)
-          .send("Location not found");
+        res.status(404).send("Location not found");
 
-        return
+        return;
       }
 
-      await location.destroy()
+      await location.destroy();
 
-      res.status(200).send()
-    } catch(error) {
+      res.status(200).send();
+      emitChannelEvent(io, DELETE, location.id)
+    } catch (error) {
       console.error(`Error: location:delete ${locationId}:`, error);
 
       res
