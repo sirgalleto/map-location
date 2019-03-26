6e8682b298e0fb9afa16ac699a238f181506e751
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 00:30:11 -0600

    🚧Add location model definition

---
+++ packages/api/models/location.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const Location = sequelize.define("Location", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    long: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    isOpen: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  }, {
+      timestamps: false,
+  });
+
+  return Location;
+};
