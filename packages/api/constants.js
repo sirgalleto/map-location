5461e4ebccc9cc0b17e5c6e9072dc5bcbc229f3b
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 16:49:39 -0600

    🚧Move constants to a single file

---
+++ packages/api/constants.js
@@ -0,0 +1,13 @@
+const SEQUELIZE_VALIDATION_ERROR = "SequelizeValidationError";
+const LOCATIONS_CHANNEL_NAME = "/locations";
+const CREATE = "CREATE";
+const UPDATE = "UPDATE";
+const DELETE = "DELETE";
+
+module.exports = {
+  SEQUELIZE_VALIDATION_ERROR,
+  LOCATIONS_CHANNEL_NAME,
+  CREATE,
+  UPDATE,
+  DELETE,
+}
