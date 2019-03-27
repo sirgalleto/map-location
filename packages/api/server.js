3efe71bb126e15995089249e03dfc39d0439bf44
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 23:57:29 -0600

    🐛Enable cors

--- packages/api/server.js
+++ packages/api/server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+var cors = require("cors");
 const location = require("./routes/location");
 
-app.set('io', io)
+app.set("io", io);
 app.use(express.json());
+app.use(cors());
 app.use("/location", location);
 
-module.exports = server
+module.exports = server;
