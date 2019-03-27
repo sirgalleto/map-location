cb4eac712f08f61733f470d468ff8d007905cc05
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-27 14:29:50 -0600

    🚧Add mapbox components listing the markers from the locations state

--- packages/webclient/src/App.vue
+++ packages/webclient/src/App.vue
@@ -1,22 +1,54 @@
 <template>
-  <div id="app">
-    <span v-if="error">
-      {{ error }}
-    </span>
-    <ul>
-      <li v-for="location in locations" :key="location.id">
-        {{ location.name }} : {{location.lat}},{{location.long}} : {{ location.isOpen ? 'open' : 'closed' }}
-      </li>
-    </ul>
-  </div>
+  <MglMap :accessToken="accessToken" :mapStyle="mapStyle">
+     <MglMarker v-for="location in locations" :key="location.id" :coordinates="composeLongLat(location)" color="blue" />
+  </MglMap>
 </template>
 
 <script>
+import Mapbox from 'mapbox-gl'
+import { MglMap, MglMarker } from 'vue-mapbox'
 import { mapActions, mapState } from 'vuex'
+
+const mapboxAccessToken = process.env['VUE_APP_MAPBOX_ACCESS_TOKEN']
 
 export default {
   name: 'app',
+  components: {
+    MglMap,
+    MglMarker
+  },
+  data() {
+    const mapStyle = {
+      version: 8,
+      name: "Mapbox Streets",
+      sprite: "mapbox://sprites/mapbox/streets-v8",
+      glyphs: "mapbox://fonts/mapbox/{fontstack}/{range}.pbf",
+      sources: {
+        'mapbox-streets': {
+          type: 'vector',
+          url: 'mapbox://mapbox.mapbox-streets-v6'
+        }
+      },
+      layers: [
+        {
+          id: 'water',
+          source: 'mapbox-streets',
+          'source-layer': 'water',
+          type: 'fill',
+          paint: {
+            'fill-color': '#00ffff'
+          }
+        }
+      ]
+    }
+    
+    return {
+      mapStyle,
+      accessToken: mapboxAccessToken,
+    }
+  },
   created() {
+    this.mapbox = Mapbox
     this.fetchLocations()
   },
   computed: {
@@ -28,18 +60,17 @@ export default {
   methods: {
     ...mapActions('locations', {
       fetchLocations: 'fetchLocations',
-    })
+    }),
+    composeLongLat(location) {
+      return [location.long, location.lat]
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.mgl-map-wrapper {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
