f6bb872a77f0a4a2e1b31e9b7846d1f65b78379e
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 23:28:03 -0600

    🚧Initialize Vuex store

---
+++ packages/webclient/src/store/locations.js
@@ -0,0 +1,20 @@
+const initialState = {
+  isFetching: false,
+  error: null,
+  items: [],
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+  namespaced: true,
+  state: initialState,
+  mutations,
+  actions,
+}
