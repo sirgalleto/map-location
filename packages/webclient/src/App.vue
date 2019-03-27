96f7a53bdc5d6894a1176ba6a6f2e5017c40757e
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-26 23:59:07 -0600

    🚧List the locations from the store

--- packages/webclient/src/App.vue
+++ packages/webclient/src/App.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="app">
-
+    <span v-if="error">
+      {{ error }}
+    </span>
+    <ul>
+      <li v-for="location in locations" :key="location.id">
+        {{ location.name }} : {{location.lat}},{{location.long}} : {{ location.isOpen ? 'open' : 'closed' }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
   name: 'app',
+  created() {
+    this.fetchLocations()
+  },
+  computed: {
+    ...mapState('locations', {
+      locations: state => state.items,
+      error: state => state.fetchingError
+    })
+  },
+  methods: {
+    ...mapActions('locations', {
+      fetchLocations: 'fetchLocations',
+    })
+  }
 }
 </script>
 
