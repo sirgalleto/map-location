8f89abe5b505cb6574b5dfe9077af0592506e6fd
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 00:31:21 -0600

    🚧Add create, update, and delete services

--- packages/webclient/src/services/location.js
+++ packages/webclient/src/services/location.js
@@ -1,11 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseUrl = "http://0.0.0.0:3000/location";
+const baseUrl = 'http://0.0.0.0:3000/location';
 
 export default {
   async getAll() {
     const request = await axios.get(baseUrl);
 
     return request.data;
-  }
+  },
+
+  async create(location) {
+    const request = await axios.put(baseUrl, location);
+
+    return request.data;
+  },
+
+  async update(location) {
+    const request = await axios.put(baseUrl, location);
+
+    return request.data;
+  },
+
+  async delete(locationId) {
+    const request = await axios.delete(`${baseUrl}/${locationId}`);
+
+    return request.data;
+  },
 };
