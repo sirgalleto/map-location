1274eaa48cfffde432207c4964351c19d15bbf87
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 02:44:10 -0600

    ✨Add the location info in the Marker PopUp

--- packages/webclient/src/App.vue
+++ packages/webclient/src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <MglMap
+  <VApp>
+    <MglMap
     :access-token="accessToken"
     :map-style="mapStyle"
-  >
-    <MglMarker
-      v-for="location in locations"
-      :key="location.id"
-      :coordinates="composeLongLat(location)"
-      :color="location.isOpen ? '#4B45E7' : '#EF5350'"
-    />
-  </MglMap>
+    >
+      <MglMarker
+        v-for="location in locations"
+        :key="location.id"
+        :coordinates="composeLongLat(location)"
+        :color="location.isOpen ? '#4B45E7' : '#EF5350'"
+        @added="popupAdded">
+        <MglPopup anchor="top">
+          <MarkerInfo :name="location.name" :lat="location.lat" :long="location.long" :isOpen="location.isOpen"/>
+        </MglPopup>
+      </MglMarker>
+    </MglMap>
+  </VApp>
 </template>
 
 <script>
 import Mapbox from 'mapbox-gl';
-import { MglMap, MglMarker } from 'vue-mapbox';
+import { MglMap, MglMarker, MglPopup } from 'vue-mapbox';
+import MarkerInfo from '@/components/MarkerInfo.vue';
 import { mapActions, mapState } from 'vuex';
+
 
 const mapboxAccessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -24,6 +32,8 @@ export default {
   components: {
     MglMap,
     MglMarker,
+    MglPopup,
+    MarkerInfo,
   },
   data() {
     const mapStyle = 'mapbox://styles/mapbox/dark-v10';
@@ -33,7 +43,6 @@ export default {
       accessToken: mapboxAccessToken,
     };
   },
-
   computed: {
     ...mapState('locations', {
       locations: state => state.items,
@@ -53,6 +62,14 @@ export default {
     composeLongLat(location) {
       return [location.long, location.lat];
     },
+    // Hack to start popups as closed, a glitch in the start of the page is expected
+    popupAdded(event) {
+      // When the event is triggered the popup is still not associated
+      // The setTimeout queue this action as last
+      setTimeout(() => {
+        event.marker.togglePopup()
+      })
+    }
   },
 };
 </script>
---
+++ packages/webclient/src/components/MarkerInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="!editMode">
+    <h3>
+      {{ name }}
+    </h3>
+    <small>
+      {{ lat }},{{ long }} <br/>
+      <b>{{ isOpen ? 'open' : 'closed' }}</b>
+    </small>
+  </div>
+  <div v-else>
+    Hi, I'm in edit mode!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MarkerInfo',
+  props: {
+    editMode: {
+      default: false,
+      required: false,
+      type: Boolean,
+    },
+    name: {
+      default: '',
+      type: String,
+      required: true,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    long: {
+      type: Number,
+      required: true,
+    },
+    isOpen: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
