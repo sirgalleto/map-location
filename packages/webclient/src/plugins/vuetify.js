6aa520b4aa352ec39ca1f70bd4075e62fe79bd3f
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 02:18:12 -0600

    🔧Add vuetify configuration

---
+++ packages/webclient/src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#4B45E7',
+    secondary: '#EF5350',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107',
+  },
+  iconfont: 'md',
+});
