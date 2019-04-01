cf5179124d83817bfade316f3bc875b5fd538dc3
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 23:04:27 -0600

    🎨Run server on 0.0.0.0 as address

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,7 +1,8 @@
 const server = require('./server');
 
 const port = 3000;
+const address = '0.0.0.0';
 
-server.listen(port, () => {
-  console.info(`App listennin on port ${port}`);
+server.listen(port, address, () => {
+  console.info(`App listennin on  ${address}:${port}`);
 });
