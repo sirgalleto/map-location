8e632af8ff81062652dc2f141a348496e1189cbb
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 20:04:28 -0600

    ✨Add delection confirmation

--- packages/webclient/src/components/MarkerInfo.vue
+++ packages/webclient/src/components/MarkerInfo.vue
@@ -1,35 +1,80 @@
 <template>
   <div class="marker-info-body">
-    <h3>
-      {{ name }}
-    </h3>
-    <small>
-      {{ lat }},{{ long }} <br>
-      <b>{{ isOpen ? 'open' : 'closed' }}</b>
-    </small>
+    <VCard>
+      <VCardTitle primary-title>
+        <div>
+          <h3 class="headline mb-0">
+            {{ location.name }}
+          </h3>
+          <div>
+            {{ location.lat }},{{ location.long }}
+          </div>
+          <b>{{ location.isOpen ? 'open' : 'closed' }}</b>
+        </div>
+      </VCardTitle>
+      <VCardActions>
+        <VBtn
+          flat
+          small
+          color="gray"
+          @click="$emit('edit', location)"
+        >
+          Edit
+        </VBtn>
+        <VBtn
+          flat
+          small
+          color="red"
+          @click.stop="onDelete"
+        >
+          Delete
+        </VBtn>
+      </VCardActions>
+    </VCard>
+    <Confirm
+      :title="`Do you want to remove the location ${location.name}?`"
+      :show="showDeleteConfirmation"
+      @cancel="onCancelDelete"
+      @confirm="onConfirmDelete"
+    >
+      You won't longer see this location
+    </Confirm>
   </div>
 </template>
 
 <script>
+import Confirm from '@/components/Confirm.vue';
+
 export default {
   name: 'MarkerInfo',
+  components: {
+    Confirm,
+  },
   props: {
-    name: {
-      default: '',
-      type: String,
+    location: {
       required: true,
+      type: Object,
+      default: () => {},
     },
-    lat: {
-      type: Number,
-      required: true,
+  },
+  data() {
+    return {
+      showDeleteConfirmation: false,
+    };
+  },
+  methods: {
+    toggleConfirmation() {
+      this.showDeleteConfirmation = !this.showDeleteConfirmation;
     },
-    long: {
-      type: Number,
-      required: true,
+    onDelete() {
+      this.toggleConfirmation();
     },
-    isOpen: {
-      type: Boolean,
-      required: true,
+    onCancelDelete() {
+      this.toggleConfirmation();
+    },
+    onConfirmDelete() {
+      this.toggleConfirmation();
+      this.$emit('delete', this.location);
     },
   },
 };
@@ -38,5 +83,14 @@ export default {
 <style scoped>
 .marker-info-body {
   min-width: 150px;
+}
+</style>
+
+
+<style>
+/* Not scoped to override mapbox styles */
+.mapboxgl-popup-content {
+  min-width: 200px;
+  padding: 0;
 }
 </style>
