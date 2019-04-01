c3b9c5cf38d497cbdbcb15f24cddc15ebe1e5ffd
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 20:46:29 -0600

    🐛Fix tests route

--- packages/webclient/jest.config.js
+++ packages/webclient/jest.config.js
@@ -1,23 +1,14 @@
 module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'vue',
-  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    '/node_modules/',
-  ],
+  transformIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testMatch: [
-    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
-  ],
+  testMatch: ['<rootDir>/__tests__/**/*.js'],
   testURL: 'http://localhost/',
 };
