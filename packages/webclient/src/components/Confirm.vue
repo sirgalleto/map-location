ddf98c3f7c530db48e90614c59e292685d9f5cea
sirgalleto <sebastian.osorio@wizeline.com>
2019-03-31 20:04:11 -0600

    ✨Create confirm component

---
+++ packages/webclient/src/components/Confirm.vue
@@ -0,0 +1,53 @@
+<template>
+  <VDialog
+    v-model="show"
+    persistent
+    max-width="290"
+  >
+    <VCard>
+      <VCardTitle class="headline">
+        {{ title }}
+      </VCardTitle>
+
+      <VCardText>
+        <slot />
+      </VCardText>
+
+      <VCardActions>
+        <VSpacer />
+
+        <VBtn
+          flat="flat"
+          @click="$emit('cancel')"
+        >
+          Cancel
+        </VBtn>
+
+        <VBtn
+          color="red"
+          flat="flat"
+          @click="$emit('confirm')"
+        >
+          Ok
+        </VBtn>
+      </VCardActions>
+    </VCard>
+  </VDialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+};
+</script>
